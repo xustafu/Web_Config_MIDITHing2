250a9ee04d0a36c156d7136c29c8b0737b00858f
Hugo Vazquez Alonso <hugobraulio@gmail.com>
2023-05-08 11:27:20 +0200

    removed dropdown arrow from voice selector (now voice tag)

--- src/includes/box/bodies/voice/osc.php
+++ src/includes/box/bodies/voice/osc.php
@@ -4,7 +4,7 @@
   <!-- OSCILLATOR VOICE -->
   <div class="body-selector-parent input-wrap" id="osc-voice-<?=$port_id?>">
     <h3 class="box-body-select-title inline">Voice</h3>
-    <label for="osc-voice-input-<?=$port_id?>" class="box-selector-label round-sm inline"><?=$port_id?></label>
+    <label for="osc-voice-input-<?=$port_id?>" class="box-selector-label round-sm inline voice-tag"><?=$port_id?></label>
   </div>
   <!-- OSCILLATOR VOICE END -->
 
--- src/includes/box/bodies/voice/velocity.php
+++ src/includes/box/bodies/voice/velocity.php
@@ -4,7 +4,7 @@
   <!-- VELOCITY VOICE -->
   <div class="body-selector-parent input-wrap" id="velocity-voice-<?=$port_id?>">
     <h3 class="box-body-select-title inline">Voice</h3>
-    <label for="velocity-voice-input-<?=$port_id?>" class="box-selector-label round-sm inline"><?=$port_id?></label>
+    <label for="velocity-voice-input-<?=$port_id?>" class="box-selector-label round-sm inline voice-tag"><?=$port_id?></label>
   </div>
   <!-- VELOCITY VOICE END -->
 
