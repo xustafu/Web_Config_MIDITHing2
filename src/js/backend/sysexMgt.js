62c66fecbc52911ed9effcd04ba8c9aaa387d670
Hugo Vazquez Alonso <hugobraulio@gmail.com>
2024-07-15 17:01:25 +0200

    another fix for #82, some edge cases don't work

--- src/js/backend/sysexMgt.js
+++ src/js/backend/sysexMgt.js
@@ -376,7 +376,7 @@ export function sendSysex(dtype, number, dparam, value, is_global_adsr) {
     number = number - 18
   var port = DeviceConfig.ports[number];
   var param = port.param;
-  if (port.isAddToVoice || is_port_midich) {
+  if (is_port_midich) { // || port.isAddToVoice)
     for (var i = 0; i < DeviceConfig.ports.length; i++) {
       let p = DeviceConfig.ports[i];
       if (p.voice == param && i != port.port_num - 1) {
