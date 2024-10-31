0ad925a5f0a7d1c448d4575871e8b3781468edff
Hugo Vazquez Alonso <hugobraulio@gmail.com>
2024-10-31 23:16:24 +0100

    Test 1.3 > credits and title changed

--- src/includes/banner.php
+++ src/includes/banner.php
@@ -19,7 +19,7 @@
     </svg>
   </figure>
 
-  <h1 class="banner-title"><span id="title">MIDI Thing 2 fw 1.1</span><span class="banner-subtitle"> | Editor</span></h1>
+  <h1 class="banner-title"><span id="title">MIDI Thing 2 FW V1.2</span><span class="banner-subtitle"> | Editor</span></h1>
 
   <figure class="live-button">
     <svg
